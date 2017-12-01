6df9662f4c62440a59839b539e7cdf8633d4f4ed
unique-pear486 <unique-pear486@users.noreply.github.com>
2017-12-01 20:37:06 +1100

    Add Tile deck behaviour

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
   var basementDiv = $("#basement");
   var groundDiv = $("#ground");
   var upperDiv = $("#upper");
+  var tileImg = $("#tile-img");
 
   $(".object").draggable({grid:[300,300]});
   $(".floor").draggable();
@@ -13,6 +14,20 @@
       axis:"x",
       containment: "#tracker-marker-container"
     });
+
+  // Set Tile deck behaviour
+  $("#tile-deck")
+    .hover(setTileImg("img/Tile_bgu.png"), setTileImg("img/Tile_.png"))
+    .click(getTile({upper: true, ground: true, basement: true}));
+  $("#tile-upper")
+    .hover(setTileImg("img/Tile_u.png"), setTileImg("img/Tile_bgu.png"))
+    .click(getTile({upper: true}));
+  $("#tile-ground")
+    .hover(setTileImg("img/Tile_g.png"), setTileImg("img/Tile_bgu.png"))
+    .click(getTile({ground: true}));
+  $("#tile-basement")
+    .hover(setTileImg("img/Tile_b.png"), setTileImg("img/Tile_bgu.png"))
+    .click(getTile({basement: true}));
 
   function selectBasement() {
     floorsDiv.addClass("hidd");
@@ -25,5 +40,16 @@
   function selectUpper() {
     floorsDiv.addClass("hidd");
     upperDiv.removeClass("hidd");
+  }
+  function setTileImg(source) {
+    return function() {
+      tileImg.attr('src', source);
+    };
+  }
+  function getTile(options) {
+    return function() {
+      console.log("getTile", options);
+      return false;
+    };
   }
 })(jQuery);
